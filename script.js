84b4bc7378aa1ecbff4c320f622f222247f318ab
Varun Singh <109689424+vrn-singh@users.noreply.github.com>
2026-02-11 01:37:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesBtn").onclick = function () {
+    document.getElementById("response").innerText = "Yay! 💕";
+};
+
+document.getElementById("noBtn").onclick = function () {
+    document.getElementById("response").innerText = "Oh no 😢";
+};
